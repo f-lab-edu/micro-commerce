ba45eb158d7600ea3c35167e48783805b38a67b7
chan <uchan01001@gmail.com>
2024-02-20 02:30:20 +0900

    [MEMBER-SERVICE] RequestMapping 오타 수정 & docker-compose.yml 수정

--- micro-commerce-member/src/main/java/com/microcommerce/member/presentation/MemberController.java
+++ micro-commerce-member/src/main/java/com/microcommerce/member/presentation/MemberController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/users")
+@RequestMapping("/api/v1/members")
 @RestController
 public class MemberController {
 
